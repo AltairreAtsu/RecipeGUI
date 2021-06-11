c8aa01385fa4b8729143e5ba6b14c1233090731a
Marc Taurasi <marcataurasi@gmail.com>
2021-06-11 10:54:31 -0600

    Create UI for Install Wizard, currently awaiting logic implementation

---
+++ RecipeGUI/Install Wizard/InstallStage1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RecipeGUI.Install_Wizard
+{
+	/// <summary>
+	/// Interaction logic for InstallStage1.xaml
+	/// </summary>
+	public partial class InstallStage1 : UserControl
+	{
+		public InstallStage1()
+		{
+			InitializeComponent();
+		}
+	}
+}
